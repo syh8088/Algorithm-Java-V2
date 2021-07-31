4d170e79e2377d2244f5f8c64616fe7d1a2495bc
syh8088 <syh8088@gmail.com>
2021-07-31 23:37:55 +0900

    2. 공통원소구하기(two pointers algorithm)
    2. 공통원소구하기(two pointers algorithm)

--- src/TowPointersAndSlidingWindow/Q_02/Main.java
+++ src/TowPointersAndSlidingWindow/Q_02/Main.java
@@ -1,11 +1,13 @@
-package TowPointersAndSlidingWindow.Q_01;
+package TowPointersAndSlidingWindow.Q_02;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        
 /*        Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int[] arr = new int[n];
@@ -19,14 +21,13 @@ public class Main {
 
         for (int i = 0; i < n; i++) {
             arr2[i] = in.nextInt();
-        }
-        */
+        }*/
 
-        int n = 3;
-        int[] arr = { 1, 3, 5 };
+        int n = 5;
+        int[] arr = { 1, 3, 9, 5, 2 };
 
         int m = 5;
-        int[] arr2 = { 2, 3, 6, 7, 9 };
+        int[] arr2 = { 3, 2, 5, 7, 8 };
 
         Main main = new Main();
 
@@ -42,6 +43,23 @@ public class Main {
 
        ArrayList<Integer> answer = new ArrayList<>();
 
+        Arrays.sort(arr);
+        Arrays.sort(arr2);
+
+        int p1 = 0, p2 = 0;
+        while (p1 < n && p2 < m) {
+            if (arr[p1] < arr2[p2]) {
+                p1++;
+            } else if (arr[p1] == arr2[p2]) {
+                answer.add(arr[p1]);
+                p1++;
+                p2++;
+            } else {
+                p2++;
+            }
+        }
+
         return answer;
     }
+
 }
