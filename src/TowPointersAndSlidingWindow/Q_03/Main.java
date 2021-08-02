09a551af96f5ef9cce98123e7c74e3077ca0c9b2
syh8088 <syh8088@gmail.com>
2021-08-02 23:49:57 +0900

    3. 최대 매출(Sliding window)
    3. 최대 매출(Sliding window)

---
+++ src/TowPointersAndSlidingWindow/Q_03/Main.java
@@ -0,0 +1,37 @@
+package TowPointersAndSlidingWindow.Q_03;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+/*        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+*/
+
+        int n = 10;
+        int k = 3;
+        int[] arr = { 12, 15, 11, 20, 25, 10, 20, 19, 13, 15 };
+
+        Main main = new Main();
+        System.out.print(main.solution(n, k, arr));
+    }
+
+    // 내가 풀어본거
+    public int solution(int n, int k, int[] arr) {
+
+        int answer = 0, sum = 0;
+
+
+        return answer;
+    }
+
+}
