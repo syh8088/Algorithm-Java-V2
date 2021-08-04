afdf643a9929e118ebb56ed84bee4ea402beb052
syh8088 <syh8088@gmail.com>
2021-08-04 23:17:35 +0900

    4. 연속부분수열(복합적 문제)
    4. 연속부분수열(복합적 문제)

---
+++ src/TowPointersAndSlidingWindow/Q_04/Main.java
@@ -0,0 +1,33 @@
+package TowPointersAndSlidingWindow.Q_04;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+/*        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+*/
+
+        int n = 8;
+        int m = 6;
+        int[] arr = { 1, 2, 1, 3, 1, 1, 1, 2 };
+
+        Main main = new Main();
+        System.out.print(main.solution(n, m, arr));
+    }
+
+    // 내가 풀어본거
+    public int solution(int n, int m, int[] arr) {
+
+        int answer = 0, sum = 0, lt = 0;
+        
+        return answer;
+    }
+
+}
