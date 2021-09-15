7f7eaac5fa65991a4410e183f934ddd8ab61765e
SeoYangHun <syh8088@gmail.com>
2021-09-15 10:29:56 +0900

    1. 올바른 괄호
    1. 올바른 괄호

---
+++ src/StackAndQueue/Main.java
@@ -0,0 +1,30 @@
+package StackAndQueue;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+       /* String str = in.next();*/
+
+
+        String str = "(()(()))(()";
+
+        Main main = new Main();
+        System.out.print(main.solution(str));
+    }
+
+    // 내가 풀어본거
+    public String solution(String str) {
+
+        String answer = "YES";
+
+
+        return answer;
+    }
+
+}
